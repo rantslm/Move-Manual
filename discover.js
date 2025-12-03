06e70211a238309b3e4b79c3062b3444f1c9b147
rantslm <leilanirants@gmail.com>
2025-12-03 02:41:52 -0500

    fix: updated API config and fetch exercises

--- discover.js
+++ discover.js
@@ -10,14 +10,15 @@ if (!searchInput || !resultsGrid) {
   console.warn('discover.js: required DOM elements not found.');
 } else {
   //  API config 
+  // my RapidAPI key from ExerciseDB docs
+  const API_KEY = '0e1da515dcmshe7ee7a4b14f6041p19391fjsn72a0f1a7208d';
   // Base URL for ExerciseDB via RapidAPI
   const EXERCISE_API_URL = 'https://exercisedb.p.rapidapi.com/exercises';
 
-  // TODO: replace 'YOUR_RAPIDAPI_KEY_HERE' with your actual RapidAPI key.
   const apiOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'YOUR_RAPIDAPI_KEY_HERE',
+      'X-RapidAPI-Key': API_KEY,
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
     },
   };
@@ -31,6 +32,9 @@ if (!searchInput || !resultsGrid) {
       const response = await fetch(EXERCISE_API_URL, apiOptions);
 
       if (!response.ok) {
+      
+        const text = await response.text();
+        console.error('HTTP error', response.status, text);
         throw new Error(`HTTP error ${response.status}`);
       }
 
@@ -42,11 +46,8 @@ if (!searchInput || !resultsGrid) {
       return [];
     }
   }
-
-  /* Initial boot function for the Discover page. */
-  
   (async function initDiscover() {
     const allExercises = await fetchExercises();
-    console.log('Sample exercises:', allExercises.slice(0, 3));
+    console.log("data received:", allExercises);
   })();
 }
